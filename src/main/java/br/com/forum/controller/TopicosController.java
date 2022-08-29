dddca672d24629bbf71ccc2e3cb3c291543a0ed2
Victoria Alves <vic.alves200216@gmail.com>
2022-08-29 11:12:03 -0300

    Ordenacao

--- src/main/java/br/com/forum/controller/TopicosController.java
+++ src/main/java/br/com/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,10 +42,10 @@ public class TopicosController {
 	@GetMapping
 	//DTO -> dados que saem da api
 	public Page<TopicoDto> lista(@RequestParam(required=false) String nomeCurso,
-			@RequestParam int pagina,@RequestParam int qtd) {
+			@RequestParam int pagina,@RequestParam int qtd, @RequestParam String ordenacao) {
 		
-		//Fazendo paginação
-		Pageable paginacao = PageRequest.of(pagina, qtd);
+		//Fazendo paginação de ordem crescente
+		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC, ordenacao);
 		
 		//Adicionando filtro de pesquisa
 		if(nomeCurso == null) {
