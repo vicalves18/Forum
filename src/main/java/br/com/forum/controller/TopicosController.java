df83d30eed9f290ad6a79254144b547e6e02baba
Victoria Alves <vic.alves200216@gmail.com>
2022-08-29 11:02:46 -0300

    Adicionando paginação ao metodo Listar

--- src/main/java/br/com/forum/controller/TopicosController.java
+++ src/main/java/br/com/forum/controller/TopicosController.java
@@ -1,13 +1,13 @@
 package br.com.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
-
 import javax.transaction.Transactional;
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +16,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
-
 import br.com.forum.controller.dto.DetalhesDoTopicoDto;
 import br.com.forum.controller.dto.TopicoDto;
 import br.com.forum.controller.form.AtualizacaoTopicoForm;
@@ -40,17 +40,22 @@ public class TopicosController {
 	
 	@GetMapping
 	//DTO -> dados que saem da api
-	public List<TopicoDto> lista(String nomeCurso) {
+	public Page<TopicoDto> lista(@RequestParam(required=false) String nomeCurso,
+			@RequestParam int pagina,@RequestParam int qtd) {
+		
+		//Fazendo paginação
+		Pageable paginacao = PageRequest.of(pagina, qtd);
+		
 		//Adicionando filtro de pesquisa
 		if(nomeCurso == null) {
 			//Carrega todos os topicos do banco de dados
 			//findAll() herdado do JpaRepository
-			 List<Topico> topicos = topicoRepository.findAll();
+			 Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			//transforma os objetos instanciados em lista
 			return TopicoDto.converter(topicos);
 		}else {
 			//findBy() + nome do atributo na entidade: busca pelo atributo passado
-			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
 			return TopicoDto.converter(topicos);
 		}	
 	}
