413cf79fd3c580ee67f780d754ae2ed39cd126b6
Victoria Alves <vic.alves200216@gmail.com>
2022-08-29 11:35:41 -0300

    Habilidando suporte de requisição do Spring Data

--- src/main/java/br/com/forum/ForumApplication.java
+++ src/main/java/br/com/forum/ForumApplication.java
@@ -2,8 +2,12 @@ package br.com.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+/*Habilita o suporte para o Spring pegar da requisição dos paramentros da URL,
+os campos e informaçãoes de paginação e ordenação e repassar para o Spring Data.*/
+@EnableSpringDataWebSupport
 public class ForumApplication {
 
 	public static void main(String[] args) {
