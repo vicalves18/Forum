2801199d3eaad35fb86b32fff4104d322937f44c
Victoria Alves <vic.alves200216@gmail.com>
2022-08-30 16:00:01 -0300

    Configurando Spring Security

---
+++ src/main/java/br/com/forum/config/security/SecurityConfigurations.java
@@ -0,0 +1,34 @@
+package br.com.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+	//Configuração de autenticacao(controle de acesso, login)
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+	
+	}
+	
+	//Configuração de autorizacao(URL,perfil de acesso)
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET,"/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+	}
+	
+	//Configuração de recursos estaticos(js,css,img)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	
+	}
+}
