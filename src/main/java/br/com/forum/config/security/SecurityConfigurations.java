5117ba4ddc8ec56e3e79bbe61a176bb272111f33
Victoria Alves <vic.alves200216@gmail.com>
2022-09-02 15:11:13 -0300

    Autenticação do usuario

--- src/main/java/br/com/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/forum/config/security/SecurityConfigurations.java
@@ -1,20 +1,27 @@
 package br.com.forum.config.security;
 
+import org.jboss.jandex.Main;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+	@Autowired
+	private AutenticacaoService autenticacaoService;
+	
 	//Configuração de autenticacao(controle de acesso, login)
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
+		//Passa que a clase que possue a logica de autenticacaos
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 	
 	}
 	
@@ -33,4 +40,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	public void configure(WebSecurity web) throws Exception {
 	
 	}
+	
+	/*Gera senha no formato BCrypt
+	 * public static void main(String[] args) {
+		System.out.println(new BCryptPasswordEncoder().encode("123456"));
+	}*/
 }
