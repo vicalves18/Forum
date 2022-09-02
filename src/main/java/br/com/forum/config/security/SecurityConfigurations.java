20ebfb3ba852f449160ab2da289fd30dfb64157a
Victoria Alves <vic.alves200216@gmail.com>
2022-09-02 16:53:37 -0300

    Adicionando injeção de depência AuthenticationManager

--- src/main/java/br/com/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/forum/config/security/SecurityConfigurations.java
@@ -1,9 +1,10 @@
 package br.com.forum.config.security;
 
-import org.jboss.jandex.Main;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -17,6 +18,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;;
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private AutenticacaoService autenticacaoService;
+	
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
 	
 	//Configuração de autenticacao(controle de acesso, login)
 	@Override
